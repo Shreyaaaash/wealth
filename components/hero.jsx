4b1c210986e7db21c16e1bca5a3491ca24f53817
Shreyaaaash <iamshreyash.nikam@gmail.com>
2025-01-14 17:21:40 +0530

    landing page done

---
+++ components/hero.jsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React, { useRef, useEffect } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const HeroSection = () => {
+    const imageRef = useRef(null);
+  
+    useEffect(() => {
+      const imageElement = imageRef.current;
+  
+      const handleScroll = () => {
+        const scrollPosition = window.scrollY;
+        const scrollThreshold = 100;
+  
+        if (scrollPosition > scrollThreshold) {
+          imageElement.classList.add("scrolled");
+        } else {
+          imageElement.classList.remove("scrolled");
+        }
+      };
+  
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+  return (
+    <section className="pd-200 px-4">
+        <div className="container mx-auto text-center">
+            <h1 className='="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title'> Your Finances <br />Managed <br />
+                By Intelligence</h1>
+            <p className='text-xl text-gray-700 mb-8 max-w-2xl mx-auto'>
+                An AI-driven financial platform that redefines <br />money management—track, <br />analyze,<br /> and optimize spending with premium real-time
+            </p>
+            <div>
+                <Link href="/dashboard">
+                    <Button variant="outline">Go to Dashboard</Button>
+                </Link>
+            </div>
+            <div className='hero-image-wrapper'>
+                <div ref={imageRef} className='hero-image'>
+                    <Image  src = "/banner.png"
+                    width ={1280}
+                    height={720}
+                    alt ="Dashboard Preview"
+                    className="roundeed=lg shadow-2xl border mx-auto"/>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+};
+
+export default HeroSection;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {images: {
+    domains: ['randomuser.me'], // Add the domain here
+  },};
 
 export default nextConfig;
